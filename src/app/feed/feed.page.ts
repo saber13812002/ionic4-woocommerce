062dfa7af8b6783e94ffa2d527e21389e15f5c2e
saber tabatabaee yazdi <saber.tabatabaee@gmail.com>
2019-11-13 12:49:19 +0330

    issue error convert res.body resolved

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { WoocommerceService } from '../service/woocommerce.service';
 import { concat } from 'rxjs/index';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
@@ -12,51 +14,54 @@ import { Router } from '@angular/router';
 export class FeedPage implements OnInit {
 
   products: any = [];
+  res: any
   page: number = 1;
   maxPage: number;
 
   constructor(
     public woocommerceService: WoocommerceService,
     private router: Router
-    ) { }
+  ) { }
 
   ngOnInit() {
     this.woocommerceService.getProducts(this.page)
-    .then(
-      res => {
-        this.products = res.body;
-        this.maxPage = res.headers.get('X-WP-TotalPages');
-      },
-      err => {
-        console.log(err)
-      }
-    )
+      .then(res => { this.res = res });
+
+    // .then(
+    //   (res: Object) => {
+    //     this.products = (<any>res).body;
+    //     this.maxPage = (<any>res).headers.get('X-WP-TotalPages');
+    //   },
+    //   err => {
+    //     console.log(err)
+    //   }
+    // )
   }
 
-  loadData(event){
-    if(this.page < this.maxPage){
+  loadData(event) {
+    if (this.page < this.res.headers.get('X-WP-TotalPages')) {
       console.log(event)
       setTimeout(() => {
         this.woocommerceService.getProducts(this.page + 1)
-        .then(
-          res => {
-            const newList = this.products.concat(res.body);
-            this.products = newList;
-            this.page ++;
-            event.target.complete();
-          },
-          err =>{
-            console.log(err);
-          }
-        )
+          .then(
+            res => {
+              const newList = this.products.concat((<any>res).body);
+              this.products = newList;
+              this.page++;
+              event.target.complete();
+            },
+            err => {
+              console.log(err);
+            }
+          )
       }, 500);
     }
-    else{
+    else {
       event.target.disabled = true;
     }
   }
 
-  viewDetails(productId){
+  viewDetails(productId) {
     this.router.navigate(['/details/' + productId]);
   }
 
